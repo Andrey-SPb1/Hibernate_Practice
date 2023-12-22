cd08d28310ecd6d07e8c0ffacba643ba0724d324
Andrey <honor8x98@gmail.com>
2023-12-23 00:03:33 +0300

    entity

--- src/main/java/com/andrey/HibernateRunner.java
+++ src/main/java/com/andrey/HibernateRunner.java
@@ -1,23 +1,36 @@
 package com.andrey;
 
+import com.andrey.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
 
         Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try(SessionFactory sessionFactory = configuration.buildSessionFactory();
             Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan123")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDay(LocalDate.of(1997, 8, 14))
+                    .age(26)
+                    .build();
+
+            session.persist(user);
+
+            session.getTransaction().commit();
         }
 
     }
