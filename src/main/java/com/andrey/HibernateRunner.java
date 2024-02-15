c306780c90ed9f142333092befdac0cb62df1120
Andrey <honor8x98@gmail.com>
2024-02-15 19:12:23 +0300

    Optimistic. Part 2

--- src/main/java/com/andrey/HibernateRunner.java
+++ src/main/java/com/andrey/HibernateRunner.java
@@ -20,11 +20,11 @@ public class HibernateRunner {
             session.beginTransaction();
             session1.beginTransaction();
 
-            Payment payment = session.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+            Payment payment = session.find(Payment.class, 1L);
             payment.setAmount(payment.getAmount() + 10);
 
-            Payment theSamePayment = session1.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
-            theSamePayment.setAmount(theSamePayment.getAmount() + 20);
+//            Payment theSamePayment = session1.find(Payment.class, 1L, LockModeType.OPTIMISTIC);
+//            theSamePayment.setAmount(theSamePayment.getAmount() + 20);
 
             session.getTransaction().commit();
             session1.getTransaction().commit(); // OptimisticLockException
