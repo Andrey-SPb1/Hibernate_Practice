7f7a33d39aa2acff325a51c868c06d3f521c8e25
Andrey <honor8x98@gmail.com>
2024-02-16 15:47:28 +0300

    Read only transaction

--- src/main/java/com/andrey/HibernateRunner.java
+++ src/main/java/com/andrey/HibernateRunner.java
@@ -14,25 +14,24 @@ public class HibernateRunner {
 
     public static void main(String[] args) {
         try(SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
-            Session session = sessionFactory.openSession();
-            Session session1 = sessionFactory.openSession()) {
+            Session session = sessionFactory.openSession()) {
 
+//            session.setDefaultReadOnly(true);
+//            session.setReadOnly();
             session.beginTransaction();
-            session1.beginTransaction();
 
-            session.createQuery("select p from Payment p")
-                    .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
+            session.createNativeQuery("SET TRANSACTION READ ONLY;").executeUpdate();
+
+//            session.createQuery("select p from Payment p")
+//                    .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
 //                    .setHint("javax.persistence.lock.timeout", 5000)
 //                    .setTimeout(5000)
-                    .list();
+//                    .setReadOnly(true)
+//                    .list();
 
-//            Payment payment = session.find(Payment.class, 1L, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
+            Payment payment = session.find(Payment.class, 1L);
 //            payment.setAmount(payment.getAmount() + 10);
 
-            Payment theSamePayment = session1.find(Payment.class, 1L);
-//            theSamePayment.setAmount(theSamePayment.getAmount() + 20);
-
-            session1.getTransaction().commit();
             session.getTransaction().commit();
         }
     }
