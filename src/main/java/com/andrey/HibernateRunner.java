8359a8d1e2ceef3ace258f1df44c3f55153b012b
Andrey <honor8x98@gmail.com>
2024-02-16 16:42:12 +0300

    Nontransactional data access

--- src/main/java/com/andrey/HibernateRunner.java
+++ src/main/java/com/andrey/HibernateRunner.java
@@ -1,13 +1,12 @@
 package com.andrey;
 
 import com.andrey.entity.Payment;
+import com.andrey.entity.Profile;
+import com.andrey.entity.User;
 import com.andrey.util.HibernateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
-import javax.persistence.LockModeType;
-//import javax.persistence.lock.timeout;
 
 @Slf4j
 public class HibernateRunner {
@@ -18,21 +17,28 @@ public class HibernateRunner {
 
 //            session.setDefaultReadOnly(true);
 //            session.setReadOnly();
-            session.beginTransaction();
+//            session.beginTransaction();
 
-            session.createNativeQuery("SET TRANSACTION READ ONLY;").executeUpdate();
+//            session.createNativeQuery("SET TRANSACTION READ ONLY;").executeUpdate();
 
-//            session.createQuery("select p from Payment p")
+            Profile profile = Profile.builder()
+                    .user(session.find(User.class, 1L))
+                    .street("avenue Lenina 1")
+                    .language("ru")
+                    .build();
+            session.save(profile); // it works because id generate type is identity
+
+            session.createQuery("select p from Payment p")
 //                    .setLockMode(LockModeType.PESSIMISTIC_FORCE_INCREMENT)
 //                    .setHint("javax.persistence.lock.timeout", 5000)
 //                    .setTimeout(5000)
 //                    .setReadOnly(true)
-//                    .list();
+                    .list();
 
             Payment payment = session.find(Payment.class, 1L);
 //            payment.setAmount(payment.getAmount() + 10);
 
-            session.getTransaction().commit();
+//            session.getTransaction().commit();
         }
     }
 }
