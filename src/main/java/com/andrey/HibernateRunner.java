bcc363c6bcde0e1334ff16f30496268e027c2a48
Andrey <honor8x98@gmail.com>
2024-02-29 17:20:53 +0300

    Second Level Cache. Конфигурация

--- src/main/java/com/andrey/HibernateRunner.java
+++ src/main/java/com/andrey/HibernateRunner.java
@@ -1,16 +1,10 @@
 package com.andrey;
 
 import com.andrey.entity.*;
-import com.andrey.interceptor.GlobalInterceptor;
 import com.andrey.util.HibernateUtil;
-import com.andrey.util.TestDataImporter;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.ReplicationMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
-import org.hibernate.envers.query.AuditEntity;
 
 @Slf4j
 public class HibernateRunner {
@@ -25,24 +19,6 @@ public class HibernateRunner {
                 payment.setAmount(payment.getAmount() + 10);
 
                 session.getTransaction().commit();
-            }
-
-            try(Session session2 = sessionFactory.openSession()) {
-                session2.beginTransaction();
-
-                AuditReader auditReader = AuditReaderFactory.get(session2);
-                Payment oldPayment = auditReader.find(Payment.class, 1L, 1L);
-                session2.replicate(oldPayment, ReplicationMode.OVERWRITE);
-
-                auditReader.createQuery()
-                        .forEntitiesAtRevision(Payment.class, 10L)
-                        .add(AuditEntity.property("amount").ge(450))
-                        .add(AuditEntity.property("id").ge(6L))
-                        .addProjection(AuditEntity.id())
-                        .addProjection(AuditEntity.property("amount"))
-                        .getResultList();
-
-                session2.getTransaction().commit();
             }
         }
     }
